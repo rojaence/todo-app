09f9881aa5fdefc4a777bae4e383e24481abade8
Ronny Endara <80226284+rojaence@users.noreply.github.com>
2022-10-14 00:20:30 -0500

    [UPDATE] vite config: base url and resolve alias

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import * as path from "path";
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/todo-app/",
   plugins: [react()],
   resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
-  },
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 });
