259a8648f21b1c7a0050e5a2df6b32895d6f9399
Ronny Endara <80226284+rojaence@users.noreply.github.com>
2022-10-13 23:18:48 -0500

    [UPDATE] Deploy.yaml

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import * as path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/todo-app/',
+  base: "/todo-app/",
   plugins: [react()],
   resolve: {
-    alias: [{find: '@', replacement: path.resolve(__dirname, 'src')}],
-  }
-})
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+});
