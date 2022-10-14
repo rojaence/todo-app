59d47fcdc073885c83e5b2c2525ac81e5c800f59
Ronny Endara <80226284+rojaence@users.noreply.github.com>
2022-10-13 22:24:51 -0500

    [ADD] Deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/rest-countries-api/',
   plugins: [react()],
   resolve: {
     alias: [{find: '@', replacement: path.resolve(__dirname, 'src')}],
